186f4d774c43f452848ad3fec8c2f8d37c2b36c8
mateodonino <mateodonino@gmail.com>
2024-10-17 16:11:31 -0300

    updated

---
+++ 01-tipos/02-primitivos.js
@@ -0,0 +1,6 @@
+let numero = 10; // Number
+let texto = "Hola Mundo"; // String
+let verdadero = true; // Boolean
+let falso = false; // Boolean
+let nullValue = null; // Null
+let undefinedValue = undefined; // Undefined
