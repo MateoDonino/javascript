a7e2b576ef590060197906943f42bcd80b6d6d00
mateodonino <mateodonino@gmail.com>
2024-10-17 15:53:13 -0300

    updated

---
+++ 01-tipos/01-variables.js
@@ -0,0 +1,13 @@
+let nombre = "Hola Mundo!";
+
+// Palabra reservada (let) para declarar variables.
+// Las varriables no pueden ser reasignadas o repetidas, tienen que comenzar
+// siempre con letras o guion bajo, no puede comenzar con numero
+// y no pueden tener espacios o simbolos
+// UpperCamelCasse, camelCase, snake_case.
+
+let NombreCompleto;
+let nombreCompleto;
+let nombre_completo;
+
+console.log(nombre);
